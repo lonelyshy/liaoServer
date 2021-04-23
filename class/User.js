2f22c97c33423a92cb7e2238046394c242254a98
tl <tl244739228@qq.com>
2021-04-23 17:40:02 +0800

    fix:修改用户名重复的问题

--- class/User.js
+++ class/User.js
@@ -16,6 +16,7 @@ class User{
     // this.queryUserName(name,socketId)
   }
   queryUserName(name,socketId=''){
+    console.log('name',name)
     for(let item of this.userMap){
       console.log("item",item)
       if(item[0] == socketId){
@@ -24,8 +25,9 @@ class User{
       if(this.userMap.get(item[0]).get('name') === name){
         // this.userMap.get(item[0]).set('name',name+Math.ceil(Math.random()*100))
         return '重复'
-      }
+      } 
     }
+    return '不重复'
   }
   deleteUser(socketId){//断开链接时  删除当前用户socket
     return this.userMap.delete(socketId)//删除断开链接的用户
